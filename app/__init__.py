f3c3b9fb945dacdfb71969d79647d587349502f9
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2026-04-20 09:35:23 -0300

    Atualiza o readme

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+# pega do ambiente (Render) ou usa local
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+else:
+    from config import Config
+    app.config.from_object(Config)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 from app import routes, models
